2525d37641485f580e8923e729b83403a0803388
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-07-16 21:43:38 +0200

    js edited

--- app/assets/javascripts/myJS.js
+++ app/assets/javascripts/myJS.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
 
-		$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
-		$('.rated').raty({ path: '/assets/images', readOnly: true, score: function(){return $(this).attr('data-score');} });
-	$('.alert').delay('slow').fadeOut('slow');
+	
 });

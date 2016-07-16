ec4624f0ea97d6abe497c775fb66a990e65f0539
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-07-16 14:08:00 +0200

    5.9 validation and pagination

--- app/assets/javascripts/myJS.js
+++ app/assets/javascripts/myJS.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
 
+		$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+		$('.rated').raty({ path: '/assets/images', readOnly: true, score: function(){return $(this).attr('data-score');} });
 	$('.alert').delay('slow').fadeOut('slow');
 });

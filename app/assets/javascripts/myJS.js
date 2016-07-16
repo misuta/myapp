29395748f8331cf772e5c18df4052edda1bf7ab7
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-07-16 22:28:36 +0200

    js delay edited

--- app/assets/javascripts/myJS.js
+++ app/assets/javascripts/myJS.js
@@ -1,5 +1,1 @@
-$(document).ready(function(){
-	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets/images', readOnly: true, score: function(){return $(this).attr('data-score');} });
-	$('.alert').delay('slow').fadeOut('slow');
-});
+$('.alert').delay('slow').fadeOut('slow');

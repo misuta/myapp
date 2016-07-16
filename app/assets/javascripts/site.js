cb4c3b88197e64e92b5741e45f58b7b0af4491f2
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-07-16 23:25:03 +0200

    images moved one up

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
 $(document).on('read page:load', function(){
-	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets/images', readOnly: true, score: function(){return $(this).attr('data-score');} });
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets', readOnly: true, score: function(){return $(this).attr('data-score');} });
 	$('.alert').delay('slow').fadeOut();
 });

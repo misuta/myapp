7ac1260cdf957270ff86bb54facae9d29252c228
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-07-18 16:12:52 +0200

    site.js

--- app/assets/javascripts/myJS.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets', readOnly: true, score: function(){return $(this).attr('data-score');} });
-	$('.alert').delay('slow').fadeOut();
-});
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('read page:load', function(){
+$(document).on('ready page:load', function(){
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets', readOnly: true, score: function(){return $(this).attr('data-score');} });
 	$('.alert').delay('slow').fadeOut();

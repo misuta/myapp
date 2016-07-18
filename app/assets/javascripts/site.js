97aa7add0f247f7979150731f33d8ae42dfdaf17
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-07-18 15:46:40 +0200

    read page: load

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('read page:change', function(){
+$(document).on('read page:load', function(){
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets', readOnly: true, score: function(){return $(this).attr('data-score');} });
 	$('.alert').delay('slow').fadeOut();

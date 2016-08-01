7f8e81d7438eec952eaf302f18ccdbebdb77467c
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-08-01 21:55:48 +0200

    6.11 caching

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'stripe'
 
 gem 'brakeman', '~> 3.3', '>= 3.3.3'
 
+gem 'dalli'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,10 @@
 module ProductsHelper
+
+  def ProductsHelper
+    count = Product.count
+    max_updated_at = Product.maximum(:update_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
+
+
 end

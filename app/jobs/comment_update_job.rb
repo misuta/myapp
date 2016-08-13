58a87532ed5478682ff08e4a37d26c7843f8315a
Giovanni Ceballos Contreras <misuta@hotmail.com>
2016-08-14 01:16:54 +0200

    comments revision

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -2,14 +2,12 @@ class CommentUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(comment, current_user)
-    # Do something later
-    ProductChannel.broadcast_to comment.product.id, comment: render_comment(comment, current_user)
+    ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
   end
 
   private
-  
+
     def render_comment(comment, current_user)
-      CommentsController.render(partial: 'comments/comment', locals: { comment:
-        comment, current_user: current_user})
+      CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })
     end
 end
